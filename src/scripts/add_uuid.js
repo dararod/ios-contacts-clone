18651cdc6ebe7bb354bef43fef6e9bca39d01f91
David Rodriguez <dalejorod@gmail.com>
2022-05-15 16:14:55 -0400

    Add: uids for dumbdata

--- src/scripts/add_uuid.js
+++ src/scripts/add_uuid.js
@@ -1,18 +1,25 @@
 const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
 
-const dataAsArray = JSON.parse(data);
-const dataWithIds = dataAsArray.map((contact) => ({
-  ...contact,
-  id: uuidv4(),
-}));
-const dataWithIdsAsString = JSON.stringify(dataWithIds, ' ', 2);
-
-fs.writeFile('src/static/dumbdata_with_ids.json', dataWithIdsAsString, err => {
+fs.readFile('src/static/dumbdata.json', 'utf8', (err, data) => {
   if (err) {
     console.error(err);
     return;
   }
 
-  console.log('File written with success!');
+  const dataAsArray = JSON.parse(data);
+  const dataWithIds = dataAsArray.map((contact) => ({
+    ...contact,
+    id: uuidv4(),
+  }));
+  const dataWithIdsAsString = JSON.stringify(dataWithIds, ' ', 2);
+
+  fs.writeFile('src/static/dumbdata_with_ids.json', dataWithIdsAsString, err => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+
+    console.log('File written with success!');
+  });
 });
