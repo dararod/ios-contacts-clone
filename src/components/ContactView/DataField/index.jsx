59a49f02bf5a49c99291f0d9e99599c7dfb145f0
David Rodriguez <dalejorod@gmail.com>
2022-05-20 17:24:20 -0400

    add: datafiel component and design adjustments

--- src/components/ContactView/DataField/index.jsx
+++ src/components/ContactView/DataField/index.jsx
@@ -6,12 +6,18 @@ export default function DataField() {
   return (
     <>
       <div className="data-field">
-        <h4>mobile</h4>
-        <button>850-331-1479</button>
+        <div className="data-field-mobile">
+          <h4>mobile</h4>
+          <button>850-331-1479</button>
+        </div>
+        <div>
+          <h4>home</h4>
+          <button>850-331-1479</button>
+        </div>
       </div>
-      <div className="data-field-a">
-        <h4>home</h4>
-        <button>850-331-1479</button>
+      <div id="data-field-work">
+        <h4>work</h4>
+        <button>hoovessgills@fundsgushiest.me</button>
       </div>
     </>
   )
